1343a0a5614f3d49906d29815b41cbe62a75dde0
Dhobi <sreenivasulu.dhobi@accenture.com>
2017-02-03 15:36:29 +0530

    git ext

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,10 @@
 node {
   def value = param
-  git url: 'https://github.com/sreenivasd/configserver.git'
+  git url: 'https://github.com/sreenivasd/${param}'
   def mvnHome = tool 'M3'
-  bat "${mvnHome}/bin/mvn -B -Dmaven.test.failure.ignore verify"  
+  bat "${mvnHome}/bin/mvn -B -Dmaven.test.failure.ignore verify"
+  step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
+   step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+  bat "${mvnHome}/bin/mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar"
+  bat "${mvnHome}/bin/mvn sonar:sonar -Dsonar.host.url=http://localhost:9000"
 }
